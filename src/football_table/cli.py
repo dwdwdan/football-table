1e60dcf62e728d657ebffe74c18da258314bdbb8
Daniel Walters <dan.walters5@outlook.com>
2022-07-19 12:42:56 +0100

    Load and/or create database

--- src/football_table/cli.py
+++ src/football_table/cli.py
@@ -1,2 +1,46 @@
+import sqlite3
+from pathlib import Path
+
+db_file = Path("database_test.db")
 def main():
-    print("Hello World")
+    connect_to_db()
+    conn.close()
+
+def connect_to_db():
+    create_tables = False
+    if not db_file.is_file():
+        create_tables=True
+    # conn and cur are set as globals so they can easily be used in
+    # other functions.
+    global conn
+    global cur
+    conn = sqlite3.connect(db_file, isolation_level=None)
+    cur = conn.cursor()
+    print("Connected to database")
+
+    if create_tables:
+        generate_empty_db()
+
+def generate_empty_db():
+    """Creates required tables. WARNING: THIS WILL DELETE ALL DATA"""
+    confirmation = input(f"Do you want to generate a new empty database?\n THIS WILL DELETE ALL DATA (enter Y to confirm)")
+    if str2bool(confirmation) is True:
+        try:
+            cur.execute("BEGIN TRANSACTION;")
+            cur.execute("DROP TABLE IF EXISTS games;")
+            cur.execute("DROP TABLE IF EXISTS teams;")
+            cur.execute("""CREATE TABLE games (
+                "id"	INTEGER NOT NULL UNIQUE,
+                "home_team"	INTEGER NOT NULL,
+                "away_team"	INTEGER NOT NULL,
+                "home_score"	INTEGER,
+                "away_score"	INTEGER,
+                PRIMARY KEY("id" AUTOINCREMENT))""")
+            cur.execute("""CREATE TABLE teams (
+                "id"	INTEGER NOT NULL UNIQUE,
+                "name"      TEXT,
+                PRIMARY KEY("id" AUTOINCREMENT))""")
+            cur.execute("COMMIT;")
+        except Exception as e:
+            print("ERROR: ", e)
+            cur.execute("ROLLBACK;")
